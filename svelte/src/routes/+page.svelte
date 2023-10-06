06f0049f34e950ba4d2caf17c70684d488c6351e
Alessio Grespi <56415370+AlessioGrespi@users.noreply.github.com>
2023-10-06 11:55:03 +0100

    logic and props

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,8 +1,19 @@
 <script>
 	
-    import Props from "../components/props.svelte";
+    import Logic from "../components/Logic.svelte";
+	import Logic2 from "../components/Logic2.svelte";
+	import Logic4 from "../components/Logic4.svelte";
+import Props from "../components/props.svelte";
+	import Props2 from "../components/props2.svelte";
     import Reactivity from "../components/reactivity.svelte";
     import Rickroll from "../components/rickroll.svelte";
+
+    const pkg = {
+        name: 'svelte',
+        speed: 'blazing',
+        version: 4,
+        website: 'https://svelte.dev'
+    }
 
 </script>
 
@@ -13,3 +24,11 @@
 <Props answer={42} /> <!-- How to pass in arguments into components -->
 
 <Props />
+
+<Props2 {...pkg} />
+
+<Logic />
+
+<Logic2 />
+
+<Logic4 />

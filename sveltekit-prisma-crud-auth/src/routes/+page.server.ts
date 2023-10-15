ea0765e260daee2bc9f79d1a22636ddc3d47ba62
Alessio Grespi <56415370+AlessioGrespi@users.noreply.github.com>
2023-10-15 22:51:28 +0100

    added cookies

--- sveltekit-prisma-crud-auth/src/routes/+page.server.ts
+++ sveltekit-prisma-crud-auth/src/routes/+page.server.ts
@@ -1,4 +1,3 @@
-
 import type { Actions, PageServerLoad } from "./$types"
 import { prisma } from "$lib/server/prisma"
 
@@ -11,7 +10,7 @@ export const load: PageServerLoad = async ({ locals }) => {
 }
 
 export const actions: Actions = {
-	login: async ({ request }) => {
+	login: async ({ request, cookies }) => {
 
  		const email = Object.fromEntries(await request.formData()) 
 
@@ -19,17 +18,40 @@ export const actions: Actions = {
 
 		try {
 			const users = await prisma.users.findUniqueOrThrow({ // throws error when email not existing
-				where: { email: String('b@a.com') } // To do next, accept form submission
+				where: { email: String('a@a.com') } // To do next, accept form submission
+				//where: 
 			})
 			//console.log("db read")
 			//console.log(users)
+			cookies.set("usertype", "user", {
+				path: "/",
+				httpOnly: true,
+				sameSite: "strict",
+				secure: process.env.NODE_ENV === "production",
+				maxAge: 60 * 60 * 24 * 7, // 1 week
+			})
+			cookies.set("email", "email", {
+				path: "/",
+				httpOnly: true,
+				sameSite: "strict",
+				secure: process.env.NODE_ENV === "production",
+				maxAge: 60 * 60 * 24 * 7, // 1 week
+			})
+
+
+
+
 		} catch (err) {
 			console.log("No Email of this type")
 			//console.error(err)
 			return fail(500, { message: "No Email found" })
 		}
 		return {
+			
+
 			status: 202,
+
+			
 		}
 	}
 
